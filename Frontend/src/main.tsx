16d15ef4c0098ea53045b12f17f5938b14fbac0b
Anotida Dzinotyiwei <160156839+ano4l@users.noreply.github.com>
2026-09-09 23:51:25 +0200

    feat: make VirtuHost a mobile PWA

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,6 +4,12 @@ import App from './App'
 import './index.css'
 import './enhancements.css'
 
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => {
+    void navigator.serviceWorker.register('/sw.js')
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
